c842552ed4b1398e5744d6267bb35eba12ad2d16
KillariDev <killari@gmail.com>
2024-01-05 15:56:45 +0200

    fixes

--- src/components/Logo/QueryTokenLogo.tsx
+++ src/components/Logo/QueryTokenLogo.tsx
@@ -1,14 +1,13 @@
 import { NATIVE_CHAIN_ID } from 'constants/tokens'
 import { TokenStandard } from 'graphql/data/__generated__/types-and-hooks'
 import { SearchToken } from 'graphql/data/SearchTokens'
-import { TokenQueryData } from 'graphql/data/Token'
 import { supportedChainIdFromGQLChain } from 'graphql/data/util'
 
 import AssetLogo, { AssetLogoBaseProps } from './AssetLogo'
 
 export default function QueryTokenLogo(
   props: AssetLogoBaseProps & {
-    token?: TokenQueryData | SearchToken
+    token?: SearchToken
   }
 ) {
   const chainId = props.token?.chain ? supportedChainIdFromGQLChain(props.token?.chain) : undefined
