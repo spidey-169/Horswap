d5422a07d2c9190ba51de92184dcf316143bfc1b
KillariDev <killari@gmail.com>
2024-01-08 13:40:12 +0200

    linting

--- src/graphql/data/Token.ts
+++
@@ -1,1 +0,0 @@
-export type { Chain } from './__generated__/types-and-hooks'
--- src/graphql/data/util.tsx
+++ src/graphql/data/util.tsx
@@ -2,9 +2,7 @@ import { ChainId, Currency, Token } from '@uniswap/sdk-core'
 import { AVERAGE_L1_BLOCK_TIME } from 'constants/chainInfo'
 import { nativeOnChain } from 'constants/tokens'
 import ms from 'ms'
-import { getNativeTokenDBAddress } from 'utils/nativeTokens'
-
-import { Chain, ContractInput, TokenStandard } from './__generated__/types-and-hooks'
+import { Chain, TokenStandard } from './__generated__/types-and-hooks'
 
 export enum PollingInterval {
   Slow = ms(`5m`),
