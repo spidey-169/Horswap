0f334945764adf73a57e9cfe25be7d169e45d0d8
KillariDev <killari@gmail.com>
2024-01-08 11:02:43 +0200

    no need this anymore

--- src/hooks/usePoolTickData.ts
+++
@@ -1,8 +0,0 @@
-import JSBI from 'jsbi'
-// Tick with fields parsed to JSBIs, and active liquidity computed.
-export interface TickProcessed {
-  tick: number
-  liquidityActive: JSBI
-  liquidityNet: JSBI
-  price0: string
-}
