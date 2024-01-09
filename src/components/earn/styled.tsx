517ff3f066f4d38d47ec8b855ed164449968f67e
KillariDev <killari@gmail.com>
2024-01-09 15:27:50 +0200

    linting

--- src/components/earn/styled.tsx
+++ src/components/earn/styled.tsx
@@ -27,20 +27,6 @@ export const CardBGImage = styled.span<{ desaturate?: boolean }>`
   ${({ desaturate }) => desaturate && `filter: saturate(0)`}
 `
 
-export const CardBGImageSmaller = styled.span<{ desaturate?: boolean }>`
-  background: url(${xlUnicorn});
-  width: 1200px;
-  height: 1200px;
-  position: absolute;
-  border-radius: 12px;
-  top: -300px;
-  left: -300px;
-  opacity: 0.4;
-  user-select: none;
-
-  ${({ desaturate }) => desaturate && `filter: saturate(0)`}
-`
-
 export const CardNoise = styled.span`
   background: url(${noise});
   background-size: cover;
