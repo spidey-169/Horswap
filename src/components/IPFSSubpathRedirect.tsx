ffcf24116ba9ea75876af408e97a144dc5ec0085
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2024-01-27 16:52:15 +0800

    fix generated paths on build to be relative

--- src/components/IPFSSubpathRedirect.tsx
+++ src/components/IPFSSubpathRedirect.tsx
@@ -2,30 +2,33 @@ import { CID } from 'multiformats'
 import { useEffect } from 'react'
 
 export const IPFSSubpathRedirect = () => {
-  function extractHashFromBasePath() {
-    const htmlBase = document.querySelector('base')
-    if (!htmlBase) return
-    const cidRegex = /\/ipfs\/(Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})/
-    return htmlBase.href.match(cidRegex)?.at(1)
-  }
 
-  function generateIPFSRedirect() {
-    const cidFromPath = extractHashFromBasePath()
+  useEffect(() => {
+    const hashFromBase = extractIPFSHashFromBasePath()
 
-    if (!cidFromPath) {
-      console.log('No IPFS subpath detected')
+    if (!hashFromBase) {
+      console.info('Base ref does not contain an IPFS hash')
       return
     }
 
-    const v1CidString = CID.parse(cidFromPath).toV1().toString()
-    return `${location.protocol}//${v1CidString}.ipfs.${location.host}`
-  }
-
-  useEffect(() => {
-    const redirectUrl = generateIPFSRedirect()
+    const redirectUrl = generateIpfsSubdomainUrl(hashFromBase)
     if (!redirectUrl) return
+
+    // redirect to IPFS subdomain url
     window.location.href = redirectUrl
-  }, [generateIPFSRedirect])
+  }, [])
 
   return null
+}
+
+function extractIPFSHashFromBasePath() {
+  const htmlBase = document.querySelector('base')
+  if (!htmlBase) return
+  const cidRegex = /\/ipfs\/(Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})/
+  return htmlBase.href.match(cidRegex)?.at(1)
+}
+
+function generateIpfsSubdomainUrl(cidHash: string) {
+  const cidV1String = CID.parse(cidHash).toV1().toString()
+  return `${location.protocol}//${cidV1String}.ipfs.${location.host}`
 }
